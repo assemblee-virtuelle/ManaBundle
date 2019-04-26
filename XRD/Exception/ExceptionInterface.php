56bd4bfc9b8d5c2cbd2906ab9754067645ae8ade
Michel Cadennes <michel.cadennes@assemblee-virtuelle.org>
2019-04-26 16:39:18 +0200

    Interface pour les classes d'erreurs

---
+++ XRD/Exception/ExceptionInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of the Mana bundle.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AssembleeVirtuelle\ManaBundle\XRD\Exception;
+
+/**
+ * Base exception interface.
+ *
+ * @author Michel Cadennes <michel.cadennes@assemblee-virtuelle.org>
+ */
+interface ExceptionInterface
+{
+}
