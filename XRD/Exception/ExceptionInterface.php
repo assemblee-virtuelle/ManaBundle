e85fe3d308d11258a38bb98e9cf3cd5792089889
Michel Cadennes <michel.cadennes@assemblee-virtuelle.org>
2019-05-04 19:18:06 +0200

    Update the PHPDoc header block

--- XRD/Exception/ExceptionInterface.php
+++ XRD/Exception/ExceptionInterface.php
@@ -1,18 +1,23 @@
 <?php
 
-/*
- * This file is part of the Mana bundle.
+/**
+ * This file is part of the ManaBundle, a WebFinger library for Symfony
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
+ *
+ * @package  ManaBundle
+ * @subpackage XRD
+ * @author   Michel Cadennes <michel.cadennes@assemblee-virtuelle.org>
+ * @license  https://opensource.org/licenses/GPL-3.0 GNU General Public License v3
+ * @link https://github.com/assemblee-virtuelle/ManaBundle/tree/master/XRD/README.md
+ * @version 0.1.0
  */
 
 namespace AssembleeVirtuelle\ManaBundle\XRD\Exception;
 
 /**
  * Base exception interface.
- *
- * @author Michel Cadennes <michel.cadennes@assemblee-virtuelle.org>
  */
 interface ExceptionInterface extends \Throwable
 {
