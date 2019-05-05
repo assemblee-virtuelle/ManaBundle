d049e190b07e53c75ed2445e74dbd4a8d5423d56
Michel Cadennes <michel.cadennes@assemblee-virtuelle.org>
2019-05-05 17:02:40 +0200

    A test class for the abstract class of XRD loaders (AbstractLoader)

---
+++ Tests/XRD/Loader/AbstractLoaderTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Tests\Loader;
+
+use App\Loader\AbstractLoader;
+use App\Loader\JsonLoader;
+use App\Exception\LoaderException;
+use App\XRD\XRD;
+
+use PHPUnit\Framework\TestCase;
+
+class AbstractLoaderTest extends TestCase
+{
+  protected $loader;
+
+  /*
+   * AbstractLoader is an abstract class, so we need to instanciate
+   * one of its subclasses
+   */
+  public function setUp()
+  {
+    $this->loader = new JSONLoader();
+  }
+
+  public function typeProvider()
+  {
+    return [
+      'Chaîne de caractères' => [XRD::XRD_STRING_SOURCE, XRD::XRD_STRING_SOURCE],
+      'Fichier' => [XRD::XRD_FILE_SOURCE, XRD::XRD_FILE_SOURCE]
+    ];
+  }
+
+  /**
+   * @dataProvider typeProvider
+   */
+  public function testSetType(int $given, string $expected)
+  {
+    $this->loader->type = $given;
+
+    // $this->assertObjectHasAttribute('type', $expected);
+    $this->assertEquals($this->loader->type, $expected,"Le type de source n’a pas été correctement enregistré");
+  }
+
+  /**
+   * Tries to set the 'type' property to an unsupported value
+   */
+  public function testSetUnknownType()
+  {
+    $this->expectException(LoaderException::class);
+    $this->loader->type = "Type non reconnu";
+  }
+
+  /**
+   * Tries to set a value to an invalid property
+   */
+  public function testSetUnknownProperty()
+  {
+    $this->expectException(LoaderException::class);
+    $this->loader->invalidProperty = "Oups";
+  }
+
+  /**
+   * Tries to set the 'xrd' property to an instance of class XRD
+   */
+  public function testSetXrd()
+  {
+    $this->loader->xrd = $this->createMock(XRD::class);
+
+    $this->assertInstanceOf(XRD::class, $this->loader->xrd, "L’objet de classe XRD n’a pu être affecté à la propriété 'xrd'");
+  }
+
+  /**
+   * Tries to set the 'xrd' property to an unsupported value
+   */
+  public function testSetUnknownXrd()
+  {
+    $this->expectException(LoaderException::class);
+    $this->loader->xrd = false;
+  }
+
+}
