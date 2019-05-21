156c2ac18cc8f8cdf04ef28f2ec8ec57f1419fe3
Michel Cadennes <michel.cadennes@assemblee-virtuelle.org>
2019-05-21 18:09:19 +0200

    A test class for the ‘JsonSerializer’ class

---
+++ Tests/XRD/Serializer/JsonSerializerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AssembleeVirtuelle\ManaBundle\Tests\Serializer;
+
+use AssembleeVirtuelle\ManaBundle\XRD\Serializer\Serializer;
+use AssembleeVirtuelle\ManaBundle\XRD\Loader\LoaderFactory;
+use AssembleeVirtuelle\ManaBundle\XRD\Loader\JsonLoader;
+use AssembleeVirtuelle\ManaBundle\Exception\LoaderException;
+use AssembleeVirtuelle\ManaBundle\XRD\XRD;
+use AssembleeVirtuelle\ManaBundle\XRD\Element\Link;
+
+use PHPUnit\Framework\TestCase;
+
+class JsonSerializerTest extends TestCase
+{
+
+}
